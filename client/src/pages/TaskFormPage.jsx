209a55e727720919fa9681de6e56a70a759768a7
benjaminsucasaire <bsucasaire@applying.pe>
2023-04-29 13:34:34 -0500

    ADD: Crear Tareas Taks

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,30 @@
+import {useForm} from "react-hook-form";
+import {createTask} from "../api/task.api.js";
+import {useNavigate} from "react-router-dom";
+
 export function TaskFormPage(){
+    const {register,
+        handleSubmit,
+        formState:{errors}
+    } = useForm();
+    const navigate = useNavigate();
+    const onSubmit = handleSubmit(async (data) => {
+        await createTask(data);
+        navigate("/tasks");
+    });
     return(
-        <div>Task Form Page</div>
+        <div>
+          <form onSubmit={onSubmit}>
+              <input type="text" placeholder="title"
+                     {...register("title",{required:true})}
+              />
+              {errors.title && <span>this field is required</span> }
+              <textarea rows="3" placeholder="Description"
+              {...register("description",{required:true})}
+              ></textarea>
+              {errors.Description && <span>this field is required</span> }
+              <button>Save</button>
+          </form>
+        </div>
     )
 }
