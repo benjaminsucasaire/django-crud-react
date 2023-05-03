59d704cb11233d6f0a178ab4378bceff8c56ebca
benjaminsucasaire <bsucasaire@applying.pe>
2023-05-03 16:48:41 -0500

    Ordenar carpetas

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
-from tasks import views
+from backend.tasks import views
 
 router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskView, 'tasks')
